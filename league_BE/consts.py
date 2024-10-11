50a54d42ce7c05c4616b67a3360dc8b98992ac20
JoshuaWacks <joshuawacks4@gmail.com>
2024-10-12 00:08:38 +0200

    Script to get live data. Problem with match data overriding

---
+++ league_BE/consts.py
@@ -0,0 +1,12 @@
+
+class BaseConstants:
+
+    BASE_LIVE_URL_FEED = 'https://feed.lolesports.com/livestats/v1/window/{}?startingTime={}'
+
+    TIME_DELTA_S = 60
+
+    CURRENT_STATIC_DATA_VERSION = "1451"
+
+    TEST_MATCH_ID = 111997906552170268
+
+    TEST_MIN_START_TIME = '2024-03-31 18:00:00+00:00'
